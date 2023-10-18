14e554a01c8b04715ec277c7dede2ece5e2eaf0e
adetri <emosihidup@gmail.com>
2023-10-18 15:24:59 +0700

    add requirment

--- backend/settings.py
+++ backend/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kkhk-u2^m13l0eqt$4res22#9qgcj#za)+z=)23n#m&%zka#r8'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!pip
 DEBUG = True
